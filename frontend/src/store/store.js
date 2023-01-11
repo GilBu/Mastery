677e14034c94d5000bef90e7e853ff002a28f3bd
gilbu <gilbertbuix57@gmail.com>
2023-01-11 14:35:26 -0800

    fix dependances

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/root_reducer";
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
 const middlewares = [thunk];
 if (process.env.NODE_ENV !== "production") {
   const { logger } = require("redux-logger");
